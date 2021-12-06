eb72825e170caa0b8b9c5b6a9fc01d1f08cf7015
brandoncarrasco <brancarrasco@outlook.com>
2021-12-06 14:23:33 -0500

    Add files via upload
    last commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,15 @@
 import React from "react";
 
 const Header = () => {
-    return (
-        <div class="jumbotron bg-dark text-light">
-            <h1 class="display-4">Welcome to our bookstore!</h1>
-            <p class="lead">
-                Navigate through our website to check out the most popular books available today
-            </p>
-            <hr class="my-4" />
-            <div className="row">
-                <div className="col-sm-4"><a class="btn btn-success btn-lg" href="#" role="button">
-                    About us
-                </a></div>
-                <div className="col-sm-4"><a class="btn btn-success btn-lg" href="#" role="button">
-                    Contact us
-                </a></div>
-                <div className="col-sm-4"><a class="btn btn-success btn-lg" href="#" role="button">
-                    Policies
-                </a></div>
-            </div>
-
-        </div>
-    );
+  return (
+    <div class="jumbotron bg-dark text-light">
+      <h1 class="display-4">Welcome to our bookstore!</h1>
+      <p class="lead">
+        Navigate through our website to check out the most popular books
+        available today
+      </p>
+      <hr class="my-4" />
+    </div>
+  );
 };
 export default Header;
--- src/pages/DiscoverPage/index.js
+++ src/pages/DiscoverPage/index.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import Carousel from '../../components/Carousel'
-import Header from '../../components/Header'
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Carousel from "../../components/Carousel";
+import Header from "../../components/Header";
+import "./style.css";
 const DiscoverPage = () => {
-    return (
-        <div>
-            <Navbar />
-            <Header />
-            <Carousel />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Carousel />
+    </div>
+  );
+};
 
-export default DiscoverPage
+export default DiscoverPage;
